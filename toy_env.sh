251d8bdc4903d9115ce325d285dcfbf801054ca9
Shuchang Zhou <zsc@megvii.com>
2017-06-17 17:39:10 +0800

    Add env var scripts.

---
+++ toy_env.sh
@@ -0,0 +1,9 @@
+export VOCAB_SOURCE=${HOME}/nmt_data/toy_reverse/train/vocab.sources.txt
+export VOCAB_TARGET=${HOME}/nmt_data/toy_reverse/train/vocab.targets.txt
+export TRAIN_SOURCES=${HOME}/nmt_data/toy_reverse/train/sources.txt
+export TRAIN_TARGETS=${HOME}/nmt_data/toy_reverse/train/targets.txt
+export DEV_SOURCES=${HOME}/nmt_data/toy_reverse/dev/sources.txt
+export DEV_TARGETS=${HOME}/nmt_data/toy_reverse/dev/targets.txt
+
+export DEV_TARGETS_REF=${HOME}/nmt_data/toy_reverse/dev/targets.txt
+export TRAIN_STEPS=1000
